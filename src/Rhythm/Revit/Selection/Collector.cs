85a92380f18127b4e29ea28d6594ba012c421e19
johnpierson <sixtysecondrevit@gmail.com>
2019-01-10 09:47:05 -0700

    Upload of Classes for Zerotouch selection methods
    Includes new Grid selector by Curve

---
+++ src/Rhythm/Revit/Selection/Collector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Revit.Elements;
+
+namespace Rhythm.Revit.Selection
+{
+    /// <summary>
+    /// Wrapper class for textnotes.
+    /// </summary>
+    public class Collector
+    {
+        private Collector()
+        { }
+        /// <summary>
+        /// This node will collect all elements of type from given document.
+        /// </summary>
+        /// <param name="document">The document to collect from.</param>
+        /// <param name="elementType">The element type to collect.</param>
+        /// <returns name="element">The elements.</returns>
+        /// <search>
+        /// All Elements of Type in Document
+        /// </search>
+        public static List<global::Revit.Elements.Element> ElementsOfTypeInDocument(Autodesk.Revit.DB.Document document, Type elementType )
+        {
+            FilteredElementCollector coll = new FilteredElementCollector(document);
+            List<global::Revit.Elements.Element> elems =
+                new List<global::Revit.Elements.Element>(coll.OfClass(elementType).ToElements()
+                    .Select(e => e.ToDSType(true)));
+
+            return elems;
+        }
+        /// <summary>
+        /// This node will collect all elements of the given category from given document.
+        /// </summary>
+        /// <param name="document">The document to collect from.</param>
+        /// <param name="category">The category to collect.</param>
+        /// <returns name="element">The elements.</returns>
+        /// <search>
+        /// All Elements of Category in Document
+        /// </search>
+        public static List<global::Revit.Elements.Element> ElementsOfCategoryInDocument(Autodesk.Revit.DB.Document document, global::Revit.Elements.Category category)
+        {
+            //generate the category id from the input user viewable category
+            Autodesk.Revit.DB.ElementId categoryId = new ElementId(category.Id);
+
+            FilteredElementCollector coll = new FilteredElementCollector(document);
+            List<global::Revit.Elements.Element> elems =
+                new List<global::Revit.Elements.Element>(coll.OfCategoryId(categoryId).ToElements()
+                    .Select(e => e.ToDSType(true)));
+
+            return elems;
+        }
+    }
+}
---
+++ src/Rhythm/Revit/Selection/Selection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.Revit.DB;
+using Revit.Elements;
+using Revit.GeometryConversion;
+using RevitServices.Persistence;
+using Curve = Autodesk.Revit.DB.Curve;
+using Grid = Autodesk.Revit.DB.Grid;
+using ModelCurve = Autodesk.Revit.DB.ModelCurve;
+using Point = Autodesk.DesignScript.Geometry.Point;
+using ReferencePlane = Autodesk.Revit.DB.ReferencePlane;
+
+namespace Rhythm.Revit.Selection
+{
+    /// <summary>
+    /// Wrapper class for selections.
+    /// </summary>
+    public class Selection
+    {
+        private Selection()
+        {
+        }
+
+        /// <summary>
+        /// This node will select grids along a model curve element ordered based on the start of the model curve.
+        /// This works in the active view. So whatever plan representation your grids have, that is what is used.
+        /// </summary>
+        /// <param name="modelCurve">Revit model curve to select grids along.</param>
+        /// <returns name="orderedGrids">The intersecting grids ordered from beginning to end of the line.</returns>
+        public static List<global::Revit.Elements.Grid>IntersectingGridsByModelCurve(global::Revit.Elements.ModelCurve modelCurve)
+        {
+            ModelCurve mCurve = modelCurve.InternalElement as ModelCurve;
+
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+
+            List<global::Revit.Elements.Grid>intersectingGrids = new List<global::Revit.Elements.Grid>(); 
+
+            IList<Autodesk.Revit.DB.Element> grids = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Grids).WhereElementIsNotElementType().ToElements();
+
+            foreach (var grid in grids)
+            {
+                Grid g = grid as Grid;
+                Curve c = g.GetCurvesInView(DatumExtentType.ViewSpecific,doc.ActiveView).First();
+                Curve c2 = mCurve.GeometryCurve;
+
+                Point pt1 = Point.ByCoordinates(0,0,c.GetEndPoint(0).Z);
+                Point pt2 = Point.ByCoordinates(0, 0, c2.GetEndPoint(0).Z);
+                XYZ vec = Vector.ByTwoPoints(pt2,pt1).ToRevitType();
+
+                var transformed = c2.CreateTransformed(Transform.CreateTranslation(vec));
+
+                SetComparisonResult test = c.Intersect(transformed);
+
+                if (test == SetComparisonResult.Overlap ||
+                    test == SetComparisonResult.Subset ||
+                    test == SetComparisonResult.Superset ||
+                    test == SetComparisonResult.Equal)
+                {
+                    intersectingGrids.Add(g.ToDSType(true) as global::Revit.Elements.Grid);
+                }
+                
+            }
+
+            return intersectingGrids.OrderBy(g => g.Curve.DistanceTo(modelCurve.Curve.StartPoint)).ToList();
+
+        }
+    }
+}
