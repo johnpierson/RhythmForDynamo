1d9bc36dd19871205646bb95b61339205e545d8e
johnpierson <sixtysecondrevit@gmail.com>
2019-03-20 08:01:56 -0600

    HostObject, Tag, Wall classes Open Source

---
+++ src/Rhythm/Revit/Elements/Tag.cs
@@ -0,0 +1,148 @@
+﻿using Autodesk.DesignScript.Runtime;
+using RevitServices.Persistence;
+using RevitServices.Transactions;
+using System.Collections.Generic;
+using Revit.GeometryConversion;
+using Point = Autodesk.DesignScript.Geometry.Point;
+
+namespace Rhythm.Revit.Elements
+{
+    /// <summary>
+    /// Wrapper class for tag.
+    /// </summary>
+    public class Tags
+    {
+        private Tags()
+        {
+        }
+        /// <summary>
+        /// The position of the head of tag in model coordinates (if available).
+        /// </summary>
+        /// <param name="tag">The tag to get head position from.</param>
+        /// <returns name="point">The tag's head position as a point.</returns>
+        /// <search>
+        /// Tag, Tag.Location
+        /// </search>
+        public static List<Point> GetHeadPosition(List<global::Revit.Elements.Tag> tag)
+        {
+            //declare a list for the points
+            List<Point> point = new List<Point>();
+            foreach (var t in tag)
+            {
+                var internalTag = (Autodesk.Revit.DB.IndependentTag)t.InternalElement;
+                try
+                {
+                    point.Add(internalTag.TagHeadPosition.ToPoint());
+                }
+                catch
+                {
+                    point.Add(null);
+                }
+            }
+
+            return point;
+        }
+        /// <summary>
+        /// The position of the leader end for a tag using free end leader behavior. 
+        /// </summary>
+        /// <param name="tag">The tag to get leader end position from.</param>
+        /// <returns name="point">The tag's leader end position as a point.</returns>
+        /// <search>
+        /// Tag, Tag.Location
+        /// </search>
+        public static List<Point> GetLeaderEnd(List<global::Revit.Elements.Tag> tag)
+        {
+            //declare a list for the points
+            List<Point> point = new List<Point>();
+            foreach (var t in tag)
+            {
+                var internalTag = (Autodesk.Revit.DB.IndependentTag)t.InternalElement;
+                try
+                {
+                    point.Add(internalTag.LeaderEnd.ToPoint());
+                }
+                catch
+                {
+                    point.Add(null);
+                }
+            }
+
+            return point;
+        }
+        /// <summary>
+        /// The position of the elbow of the tag's leader.  
+        /// </summary>
+        /// <param name="tag">The tag to get leader elbow position from.</param>
+        /// <returns name="point">The tag's leader elbow position as a point.</returns>
+        /// <search>
+        /// Tag, Tag.Location
+        /// </search>
+        public static List<Point> GetLeaderElbow(List<global::Revit.Elements.Tag> tag)
+        {
+            //declare a list for the points
+            List<Point> point = new List<Point>();
+            foreach (var t in tag)
+            {
+                var internalTag = (Autodesk.Revit.DB.IndependentTag)t.InternalElement;
+                try
+                {
+                    point.Add(internalTag.LeaderElbow.ToPoint());
+                }
+                catch
+                {
+                    point.Add(null);
+                }
+            }
+
+            return point;
+        }
+        /// <summary>
+        /// This will attempt to set the leader end position of the tag.
+        /// </summary>
+        /// <param name="tag">The tag to set leader end position of.</param>
+        /// <param name="location">The new location for the leader end.</param>
+        /// <search>
+        /// Tag, Tag.Location
+        /// </search>
+        public static void SetLeaderEndPosition(global::Revit.Elements.Tag tag, Point location)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Autodesk.Revit.DB.IndependentTag internalTag = (Autodesk.Revit.DB.IndependentTag)tag.InternalElement;
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            internalTag.LeaderEnd = location.ToXyz();
+            TransactionManager.Instance.TransactionTaskDone();
+        }
+        /// <summary>
+        /// This will attempt to set the head position of the tag.
+        /// </summary>
+        /// <param name="tag">The tag to set head position of.</param>
+        /// <param name="location">The new location for the head.</param>
+        /// <search>
+        /// Tag, Tag.Location
+        /// </search>
+        public static void SetHeadPosition(global::Revit.Elements.Tag tag, Point location)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Autodesk.Revit.DB.IndependentTag internalTag = (Autodesk.Revit.DB.IndependentTag)tag.InternalElement;
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            internalTag.TagHeadPosition = location.ToXyz();
+            TransactionManager.Instance.TransactionTaskDone();
+        }
+        /// <summary>
+        /// This will attempt to set the leader elbow position of the tag.
+        /// </summary>
+        /// <param name="tag">The tag to set leader elbow position of.</param>
+        /// <param name="location">The new location for the leader elbow.</param>
+        /// <search>
+        /// Tag, Tag.Location
+        /// </search>
+        public static void SetLeaderElbowPosition(global::Revit.Elements.Tag tag, Point location)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Autodesk.Revit.DB.IndependentTag internalTag = (Autodesk.Revit.DB.IndependentTag)tag.InternalElement;
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            internalTag.LeaderElbow = location.ToXyz();
+            TransactionManager.Instance.TransactionTaskDone();
+        }
+    }
+}
---
+++ src/Rhythm/Revit/Elements/Wall.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using RevitServices.Persistence;
+using Revit.Elements;
+using RevitServices.Transactions;
+
+namespace Rhythm.Revit.Elements
+{
+    /// <summary>
+    /// Wrapper class for walls.
+    /// </summary>
+    public class Walls
+    {
+        private Walls()
+        { }
+
+        /// <summary>
+        /// This node will try to check if the walls profile has been modified using logic outlined here,
+        /// http://thebuildingcoder.typepad.com/blog/2010/11/access-to-sketch-and-sketch-plane.html
+        /// </summary>
+        /// <param name="wall">The walls to check.</param>
+        /// <returns name="bool">The result.</returns>
+        /// <search>
+        /// profile, wall
+        /// </search>
+        public static bool HasEditedProfile(global::Revit.Elements.Element wall)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Autodesk.Revit.DB.Wall internalWall = (Autodesk.Revit.DB.Wall )wall.InternalElement;
+
+            Transaction trans = new Transaction(doc, "check wall");
+            TransactionManager.Instance.ForceCloseTransaction();
+            trans.Start();
+            ICollection<ElementId> ids = doc.Delete(internalWall.Id);
+            trans.RollBack();
+
+            List<global::Autodesk.Revit.DB.Element> a = new List<global::Autodesk.Revit.DB.Element>(
+                ids.Select(id => doc.GetElement(id)));
+
+            a.Where(e => e is Autodesk.Revit.DB.Sketch || e is Autodesk.Revit.DB.SketchPlane).ToArray();
+
+            bool result = a.Count > 1;
+
+            return result;
+        }
+        /// <summary>
+        /// This node will try to check if the walls profile has been modified using logic outlined here,
+        /// http://thebuildingcoder.typepad.com/blog/2010/11/access-to-sketch-and-sketch-plane.html
+        /// </summary>
+        /// <param name="wall">The walls to check.</param>
+        /// <returns name="modelCurves">The result.</returns>
+        /// <search>
+        /// profile, wall
+        /// </search>
+        public static global::Revit.Elements.Element[] EditedProfile(global::Revit.Elements.Element wall)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Autodesk.Revit.DB.Element internalElement = wall.InternalElement;
+
+            Transaction trans = new Transaction(doc, "check element");
+            TransactionManager.Instance.ForceCloseTransaction();
+            trans.Start();
+            ICollection<ElementId> ids = doc.Delete(internalElement.Id);
+            trans.RollBack();
+
+            List<global::Autodesk.Revit.DB.Element> internalWallElements = new List<global::Autodesk.Revit.DB.Element>(
+                ids.Select(id => doc.GetElement(id)));
+
+            global::Autodesk.Revit.DB.Element[] internalCurves = internalWallElements.Where(e => e is Autodesk.Revit.DB.ModelCurve).ToArray();
+
+            global::Revit.Elements.Element[] modelCurves = internalCurves.Select(e => e.ToDSType(true)).ToArray();
+
+            return modelCurves;
+        }
+    }
+}
