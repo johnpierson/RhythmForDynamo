3e3fa8850ca847943870c4ffed8a0d2abf7631b5
johnpierson <sixtysecondrevit@gmail.com>
2019-03-20 07:58:46 -0600

    Application and Document Bug Fixes
    Prepped for release this week

--- src/Rhythm/Revit/Application/Application.cs
+++ src/Rhythm/Revit/Application/Application.cs
@@ -1,5 +1,23 @@
-﻿using Autodesk.Revit.DB;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Dynamo.Applications;
+using Dynamo.Applications.ViewModel;
+using Dynamo.Controls;
+using Dynamo.Graph;
+using Dynamo.Graph.Connectors;
+using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Models;
+using Dynamo.ViewModels;
 using RevitServices.Persistence;
+using Rhythm.Utilities;
+using Object = DSCore.Object;
 
 namespace Rhythm.Revit.Application
 {
@@ -16,17 +34,20 @@ namespace Rhythm.Revit.Application
         /// <param name="filePath">The file to obtain document from.</param>
         /// <param name="audit">Choose whether or not to audit the file upon opening. (Will run slower with this)</param>
         /// <param name="detachFromCentral">Choose whether or not to detach from central upon opening. Only for RVT files. </param>
-        /// <returns name="document">The document.</returns>
+        /// <returns name="document">The document object. Primarily for use with other Rhythm nodes.</returns>
         /// <search>
         /// Application.OpenDocumentFile, rhythm
         /// </search>
-        public static Autodesk.Revit.DB.Document OpenDocumentFile(string filePath, bool audit = false, bool detachFromCentral = false)
+        public static string OpenDocumentFile(string filePath, bool audit = false, bool detachFromCentral = false)
         {
             var uiapp = DocumentManager.Instance.CurrentUIApplication;
             var app = uiapp.Application;
-            //declare open options for user to pick to audit or not
+            Document doc;
+            string docTitle = string.Empty;
+            //instantiate open options for user to pick to audit or not
             OpenOptions openOpts = new OpenOptions();
             openOpts.Audit = audit;
+            TransmittedModelOptions tOpt = TransmittedModelOptions.SaveAsNewCentral;
             if (detachFromCentral == false)
             {
                 openOpts.DetachFromCentralOption = DetachFromCentralOption.DoNotDetach;
@@ -35,26 +56,81 @@ namespace Rhythm.Revit.Application
             {
                 openOpts.DetachFromCentralOption = DetachFromCentralOption.DetachAndPreserveWorksets;
             }
+            
             //convert string to model path for open
             ModelPath modelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(filePath);
 
-            return app.OpenDocumentFile(modelPath, openOpts);
-        }     
+            try
+            {
+                docTitle = DocumentUtils.OpenDocument(modelPath, openOpts);
+            }
+            catch (Exception)
+            {
+                //nothing
+            }
+
+            return docTitle;
+        }
+
+        
+
 
         /// <summary>
         /// This node will close the given document with the option to save.
         /// </summary>
-        /// <param name="document">The document to close.</param>
+        /// <param name="document">The background opened document object, (preferably this is the title as obtained with Applications.OpenDocumentFile from Rhythm).</param>
         /// <param name="save">Do you want to save?</param>
         /// <returns name="result">Did it work?</returns>
         /// <search>
         /// Application.CloseDocument, rhythm
         /// </search>
-        public static bool CloseDocument(Autodesk.Revit.DB.Document document,bool save)
+        public static string CloseDocument(object document, bool save)
         {
-            var result = document.Close(save);
+            Document doc = DocumentUtils.RetrieveDocument(document);
+
+            string result;
+            try
+            {
+                doc.Close(save);
+                result = "closed";
+            }
+            catch (Exception)
+            {
+                result = "Error, it appears this file cannot be found in the background opened document list.";
+            }
 
             return result;
+        }
+        /// <summary>
+        /// This node provides access to all of the open documents in revit.
+        /// </summary>
+        /// <param name="runIt">Do you want to save?</param>
+        /// <returns name="documents">The documents that are currently open.</returns>
+        public static List<string> GetOpenDocuments(bool runIt)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+
+            List<string> docNames = new List<string>();
+
+            var uiApp = DocumentManager.Instance.CurrentUIApplication;
+            var app = uiApp.Application;
+
+            foreach (Document d in app.Documents)
+            {
+                if (d.Title != doc.Title)
+                {
+                    try
+                    {
+                        docNames.Add(DocumentUtils.ConvertDocument(doc));
+                    }
+                    catch (Exception)
+                    {
+                        //nothing
+                    }
+                }
+            }
+
+            return docNames;
         }
     }
 }
---
+++ src/Rhythm/Revit/Application/Document.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Autodesk.DesignScript.Runtime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.Revit.DB;
+using Revit.Elements;
+using Revit.GeometryConversion;
+using RevitServices.Persistence;
+using RevitServices.Transactions;
+using Rhythm.Utilities;
+using GlobalParameter = Autodesk.Revit.DB.GlobalParameter;
+
+namespace Rhythm.Revit.Application
+{
+    /// <summary>
+    /// Wrapper class for document nodes.
+    /// </summary>
+    public class Documents
+    {
+        private Documents()
+        { }
+        /// <summary>
+        /// This node will copy the given elements from the given linked document into the active document.
+        /// </summary>
+        /// <param name="sourceDocument">The background opened document object, (preferably this is the title as obtained with Applications.OpenDocumentFile from Rhythm).</param>
+        /// <param name="sourceInstance">The instance of the link to copy from.</param>
+        /// <param name="elements">The elements to copy.</param>
+        /// <returns name="newElements">The copied elements.</returns>
+        /// <search>
+        /// copy
+        /// </search>
+        public static List<global::Revit.Elements.Element> CopyElementsFromLinkedDocument(object sourceDocument,
+            global::Revit.Elements.Element sourceInstance, List<global::Revit.Elements.Element> elements)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Document sourceDoc = DocumentUtils.RetrieveDocument(sourceDocument);
+            //converts elements to ids in a collection
+            ICollection<ElementId> idCollection = new List<ElementId>();
+
+            foreach (global::Revit.Elements.Element element in elements)
+            {
+                idCollection.Add(element.InternalElement.Id);
+            }
+            Autodesk.Revit.DB.RevitLinkInstance internalInstance = (Autodesk.Revit.DB.RevitLinkInstance)sourceInstance.InternalElement;
+
+            //creates blank copy paste options
+            CopyPasteOptions copyOpts = new CopyPasteOptions();
+            //creates blank transform.
+            Transform transform = Transform.CreateTranslation(Vector.XAxis().ToRevitType());
+            //commits the transaction
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            ICollection<ElementId> newElementIds = ElementTransformUtils.CopyElements(sourceDoc, idCollection, doc, internalInstance.GetTransform(), copyOpts);
+            TransactionManager.Instance.TransactionTaskDone();
+            //create a new list for the new elements
+            List<global::Revit.Elements.Element> newElements = new List<global::Revit.Elements.Element>();
+            //gets the new elements
+            foreach (ElementId id in newElementIds)
+            {
+                newElements.Add(doc.GetElement(id).ToDSType(true));
+            }
+
+            return newElements;
+        }
+
+        /// <summary>
+        /// This node will copy the given elements from the given document into the active document.
+        /// </summary>
+        /// <param name="sourceDocument">The background opened document object, (preferably this is the title as obtained with Applications.OpenDocumentFile from Rhythm).</param>
+        /// <param name="elements">The elements to copy.</param>
+        /// <returns name="newElements">The copied elements.</returns>
+        /// <search>
+        /// copy
+        /// </search>
+        public static List<global::Revit.Elements.Element> CopyElementsFromDocument(object sourceDocument, List<global::Revit.Elements.Element> elements)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            Document sourceDoc = DocumentUtils.RetrieveDocument(sourceDocument);
+            //converts elements to ids in a collection
+            ICollection<ElementId> idCollection = new List<ElementId>();
+            foreach (global::Revit.Elements.Element element in elements)
+            {
+                idCollection.Add(element.InternalElement.Id);
+            }
+            //creates blank copy paste options
+            CopyPasteOptions copyOpts = new CopyPasteOptions();
+            //creates blank transform.
+            Transform transform = Transform.CreateTranslation(Vector.XAxis().ToRevitType());
+            //commits the transaction
+            TransactionManager.Instance.EnsureInTransaction(doc);
+            ICollection<ElementId> newElementIds = ElementTransformUtils.CopyElements(sourceDoc, idCollection, doc, transform, copyOpts);
+            TransactionManager.Instance.TransactionTaskDone();
+            //create a new list for the new elements
+            List<global::Revit.Elements.Element> newElements = new List<global::Revit.Elements.Element>();
+            //gets the new elements
+            foreach (ElementId id in newElementIds)
+            {
+                newElements.Add(doc.GetElement(id).ToDSType(true));
+            }
+
+            return newElements;
+        }
+
+        /// <summary>
+        /// This node will set the starting view of the document, given the view element.
+        /// </summary>
+        /// <param name="sourceDocument">The background opened document object, (preferably this is the title as obtained with Applications.OpenDocumentFile from Rhythm).</param>
+        /// <param name="view">The view to set.</param>
+        /// <returns name="result">The result.</returns>
+        /// <search>
+        /// startingView
+        /// </search>
+        public static string SetStartingView(object sourceDocument, global::Revit.Elements.Element view)
+        {
+            string result = null;
+            Document sourceDoc = DocumentUtils.RetrieveDocument(sourceDocument);
+            try
+            {
+                TransactionManager.Instance.EnsureInTransaction(sourceDoc);
+                StartingViewSettings.GetStartingViewSettings(sourceDoc).ViewId = view.InternalElement.Id;
+                TransactionManager.Instance.TransactionTaskDone();
+                result = "Success";
+            }
+            catch
+            {
+                result = "Failure";
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// This node will copy the given drafting views and their contents from the given document into the active document.
+        /// </summary>
+        /// <param name="sourceDocument">The background opened document object, (preferably this is the title as obtained with Applications.OpenDocumentFile from Rhythm).</param>
+        /// <param name="draftingViews">The drafting views to copy.</param>
+        /// <returns name="newDraftingViews">The copied drafting views with their elements.</returns>
+        /// <search>
+        /// copy
+        /// </search>
+        public static List<global::Revit.Elements.Element> CopyDraftingViewsFromDocument(object sourceDocument, List<global::Revit.Elements.Element> draftingViews)
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+
+            Document sourceDoc = DocumentUtils.RetrieveDocument(sourceDocument);
+
+            //converts elements to ids in a collection
+            ICollection<ElementId> idCollection = new List<ElementId>();
+
+            foreach (global::Revit.Elements.Element view in draftingViews)
+            {
+                idCollection.Add(view.InternalElement.Id);
+            }
+
+            TransactionManager.Instance.ForceCloseTransaction();
+            List<View> newDraftingViews =
+                ViewUtils.DuplicateDraftingViews(sourceDoc, idCollection, doc);
+            List<global::Revit.Elements.Element> vList = new List<global::Revit.Elements.Element>(newDraftingViews.Select(v => v.ToDSType(true)));
+
+            return vList;
+        }
+
+        
+    }
+
+
+}
