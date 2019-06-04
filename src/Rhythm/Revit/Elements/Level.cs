f11dfb801302856fb3e68f22b98cd071a7a24cfe
john pierson <sixtysecondrevit@gmail.com>
2019-06-04 14:26:08 -0600

    Create Level.cs
    Addition of level class that includes Levels.HasView node

---
+++ src/Rhythm/Revit/Elements/Level.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Autodesk.Revit.DB;
+using RevitServices.Persistence;
+
+namespace Rhythm.Revit.Elements
+{
+    /// <summary>
+    /// Wrapper class for levels. We make this plural because ya know Dynamo has a crashing issue with namespaces pre 2.1.0
+    /// </summary>
+    public class Levels
+    {
+        private Levels()
+        {
+        }
+
+        /// <summary>
+        /// Check to see if the level has a view created for it.😎 
+        /// </summary>
+        /// <param name="level">The level to check.</param>
+        /// <returns name="result">Does it have a level?</returns>
+        /// <search>
+        /// Level.HasView
+        /// </search>
+        public static bool HasView(global::Revit.Elements.Level level)
+        {
+            Document doc = DocumentManager.Instance.CurrentDBDocument;
+            FilteredElementCollector fec = new FilteredElementCollector(doc);
+
+            //nested this in a try catch because I want false if this fails for any reason, instead of nulls. 
+            try
+            {
+                return fec.OfClass(typeof(ViewPlan))
+                    .Cast<ViewPlan>().Any(v => v.GenLevel.Id.IntegerValue.Equals(level.Id));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
