7acc0004faae2edb2c5bac7337a5b0bfb38d21f7
john pierson <sixtysecondrevit@gmail.com>
2019-09-05 06:42:26 -0500

    Create RevitLink.cs
    Open source of Revit link methods

---
+++ src/Rhythm/Revit/Elements/RevitLink.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+
+namespace Rhythm.Revit.Elements
+{
+    /// <summary>
+    /// Wrapper class for revit links.
+    /// </summary>
+    public class RevitLink
+    {
+        private RevitLink()
+        {
+        }
+        /// <summary>
+        /// This node will obtain the selected link's document.
+        /// </summary>
+        /// <param name="linkInstance">The link to get document from.</param>
+        /// <returns name="Document">The document.</returns>
+        /// <search>
+        ///  rhythm
+        /// </search>
+        public static Autodesk.Revit.DB.Document GetDocument(global::Revit.Elements.Element linkInstance)
+        {
+            RevitLinkInstance internalLink = (RevitLinkInstance) linkInstance.InternalElement;
+            Autodesk.Revit.DB.Document linkDoc = internalLink.GetLinkDocument();
+
+            return linkDoc;
+        }
+    }
+}
