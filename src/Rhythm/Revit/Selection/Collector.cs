932d4118d086cc7ddf85495d01ae2ba223564604
john pierson <sixtysecondrevit@gmail.com>
2020-05-22 16:39:49 -0500

    Collector Revision and DocumentUtils
    This enables cross-compatibility with other (more stubborn) package creator's packages. 😁

--- src/Rhythm/Revit/Selection/Collector.cs
+++ src/Rhythm/Revit/Selection/Collector.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using Autodesk.Revit.DB;
+using Dynamo.Graph.Nodes;
 using Revit.Elements;
+using Rhythm.Utilities;
 
 namespace Rhythm.Revit.Selection
 {
     /// <summary>
-    /// Wrapper class for textnotes.
+    /// Wrapper class for collectors.
     /// </summary>
     public class Collector
     {
@@ -16,15 +18,27 @@ namespace Rhythm.Revit.Selection
         /// <summary>
         /// This node will collect all elements of type from given document.
         /// </summary>
-        /// <param name="document">The document to collect from.</param>
+        /// <param name="document">The document to collect from. Preferably the document title obtained from Applications.OpenDocumentFile.</param>
         /// <param name="elementType">The element type to collect.</param>
         /// <returns name="element">The elements.</returns>
         /// <search>
         /// All Elements of Type in Document
         /// </search>
-        public static List<global::Revit.Elements.Element> ElementsOfTypeInDocument(Autodesk.Revit.DB.Document document, Type elementType )
+        [NodeCategory("Actions")]
+        public static List<global::Revit.Elements.Element> ElementsOfTypeInDocument(object document, Type elementType )
         {
-            FilteredElementCollector coll = new FilteredElementCollector(document);
+            Document doc = null;
+            //this enables cross-compatibility with orchid documents by converting them to built in Autodesk.Revit.DB.Documents
+            if (document.GetType().ToString().Contains("Orchid"))
+            {
+                doc = DocumentUtils.OrchidDocumentToDbDocument(document);
+            }
+            else
+            {
+                doc = document as Document;
+            }
+
+            FilteredElementCollector coll = new FilteredElementCollector(doc);
             List<global::Revit.Elements.Element> elems =
                 new List<global::Revit.Elements.Element>(coll.OfClass(elementType).ToElements()
                     .Select(e => e.ToDSType(true)));
@@ -40,12 +54,24 @@ namespace Rhythm.Revit.Selection
         /// <search>
         /// All Elements of Category in Document
         /// </search>
-        public static List<global::Revit.Elements.Element> ElementsOfCategoryInDocument(Autodesk.Revit.DB.Document document, global::Revit.Elements.Category category)
+        [NodeCategory("Actions")]
+        public static List<global::Revit.Elements.Element> ElementsOfCategoryInDocument(object document, global::Revit.Elements.Category category)
         {
+            Document doc = null;
+            //this enables cross-compatibility with orchid documents by converting them to built in Autodesk.Revit.DB.Documents
+            if (document.GetType().ToString().Contains("Orchid"))
+            {
+                doc = DocumentUtils.OrchidDocumentToDbDocument(document);
+            }
+            else
+            {
+                doc = document as Document;
+            }
+
             //generate the category id from the input user viewable category
             Autodesk.Revit.DB.ElementId categoryId = new ElementId(category.Id);
 
-            FilteredElementCollector coll = new FilteredElementCollector(document);
+            FilteredElementCollector coll = new FilteredElementCollector(doc);
             List<global::Revit.Elements.Element> elems =
                 new List<global::Revit.Elements.Element>(coll.OfCategoryId(categoryId).ToElements()
                     .Select(e => e.ToDSType(true)));
---
+++ src/Rhythm/Utilities/DocumentUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using RevitServices.Persistence;
+
+namespace Rhythm.Utilities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DocumentUtils
+    {
+        private DocumentUtils()
+        {
+        }
+
+        /// <summary>
+        /// This converts orchid documents to Revit DB Documents
+        /// </summary>
+        /// <param name="docObject"></param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        public static Autodesk.Revit.DB.Document OrchidDocumentToDbDocument(object docObject)
+        {
+            //find the orchid assembly
+            Assembly sourceAssembly = Assembly.GetAssembly(docObject.GetType());
+            Type type = sourceAssembly.GetType("Orchid.RevitProject.Common.Document");
+            //find the path of the document
+            string path =  type.InvokeMember("Path", BindingFlags.Default | BindingFlags.InvokeMethod, null, null,
+                new object[] { docObject, true }).ToString();
+
+            //revit db document
+            Autodesk.Revit.DB.Document doc = null;
+
+            foreach (Autodesk.Revit.DB.Document d in DocumentManager.Instance.CurrentUIApplication.Application.Documents)
+            {
+                if (d.PathName.Equals(path))
+                {
+                    doc = d;
+                }
+            }
+            return doc;
+        }
+        /// <summary>
+        /// This converts Revit DB Documents to orchid documents.
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        public static object DbDocumentToOrchidDocument(Autodesk.Revit.DB.Document document)
+        {
+
+            //find the orchid assembly
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().First(a => a.FullName.Contains("OrchidRB") && !a.FullName.Contains("customization"));
+            Type type = assembly.GetType("Orchid.RevitProject.Common.Document");
+            //find the path of the document
+            return type.InvokeMember("BackgroundOpen", BindingFlags.Default | BindingFlags.InvokeMethod, null, null,
+                new object[] { document.PathName});
+
+           
+        }
+    }
+}
