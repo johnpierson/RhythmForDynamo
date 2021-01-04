a796735b4e7d2b9b0f21cc636bd0409e067db446
john pierson <sixtysecondrevit@gmail.com>
2021-01-04 09:55:36 -0700

    Update Selection.cs
    Added the pick selection node back

--- src/Rhythm/Revit/Selection/Selection.cs
+++ src/Rhythm/Revit/Selection/Selection.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
 using Revit.Elements;
 using Revit.GeometryConversion;
 using RevitServices.Persistence;
+using Category = Revit.Elements.Category;
 using Curve = Autodesk.Revit.DB.Curve;
+using Element = Autodesk.Revit.DB.Element;
+using GlobalParameter = Autodesk.Revit.DB.GlobalParameter;
 using Grid = Autodesk.Revit.DB.Grid;
 using ModelCurve = Autodesk.Revit.DB.ModelCurve;
 using Point = Autodesk.DesignScript.Geometry.Point;
-using ReferencePlane = Autodesk.Revit.DB.ReferencePlane;
 
 namespace Rhythm.Revit.Selection
 {
@@ -66,7 +68,78 @@ namespace Rhythm.Revit.Selection
             }
 
             return intersectingGrids.OrderBy(g => g.Curve.DistanceTo(modelCurve.Curve.StartPoint)).ToList();
+        }
+        /// <summary>
+        /// Get Null
+        /// </summary>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        public static object GetNull()
+        {
+            return null;
+        }
+        /// <summary>
+        /// Sometimes a pick selection is nicer. 😁
+        /// </summary>
+        /// <param name="runIt">Allows you to tell the node to "run". Also allows you to refresh selection.</param>
+        /// <param name="category">The category or categories to isolate to. (leave blank if you want to be able to pick anything)</param>
+        /// <param name="singleSelection">Optional input for a single item selection. Default to multiple.</param>
+        /// <returns name="pickedElements"></returns>
+        public static object Pick(bool runIt,[DefaultArgument("Rhythm.Revit.Selection.Selection.GetNull()")]List<object> category, bool singleSelection = false)
+        {
+            var doc = DocumentManager.Instance.CurrentDBDocument;
+            var uiDoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument;
 
+            if (category is null)
+            {
+                if (singleSelection)
+                {
+                    var reference = uiDoc.Selection.PickObject(ObjectType.Element);
+                    return doc.GetElement(reference.ElementId).ToDSType(true);
+                }
+
+                var references = uiDoc.Selection.PickObjects(ObjectType.Element);
+                return references.Select(r => doc.GetElement(r.ElementId).ToDSType(true)).ToList();
+            }
+            else
+            {
+                //clear the previous categories
+                CategoryNames.Clear();
+                foreach (var c in category)
+                {
+                    switch (c)
+                    {
+                        case Category cat:
+                            CategoryNames.Add(cat.Name);
+                            break;
+                        case string catString:
+                            CategoryNames.Add(catString);
+                            break;
+                    }
+                }
+                if (singleSelection)
+                {
+                    var reference = uiDoc.Selection.PickObject(ObjectType.Element, new CategorySelectionFilter());
+                    return doc.GetElement(reference.ElementId).ToDSType(true);
+                }
+                var references = uiDoc.Selection.PickObjects(ObjectType.Element, new CategorySelectionFilter());
+                return references.Select(r => doc.GetElement(r.ElementId).ToDSType(true)).ToList();
+            }
+        }
+
+        internal static List<string> CategoryNames = new List<string>();
+    }
+
+    internal class CategorySelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return Selection.CategoryNames.Contains(elem.Category.Name);
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
         }
     }
 }
