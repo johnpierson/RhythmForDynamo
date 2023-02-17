4a4259e292c078eb685316531db13d3cc7b8c34a
John <sixtysecondrevit@gmail.com>
2023-02-17 08:27:28 -0700

    Reorganization progress.

---
+++ src/RhythmUI/RhythmUI.2023/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhythmUI._2023
+{
+    public class Class1
+    {
+    }
+}
