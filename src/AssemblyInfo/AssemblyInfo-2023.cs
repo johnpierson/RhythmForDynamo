2b8607ec5c7545eabca8a9673cc48e3d31a1aaf6
John <sixtysecondrevit@gmail.com>
2023-02-17 14:18:57 -0700

    Done for now

--- src/AssemblyInfo/AssemblyInfo-2023.cs
+++ src/AssemblyInfo/AssemblyInfo-2023.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2023.3.3")]
-[assembly: AssemblyFileVersion("2023.3.3")]
+[assembly: AssemblyFileVersion("2023.33")]
--- src/RhythmUI/RhythmUI.SharedProject/Selection/SelectRoomInLinkNodeViewCustomization.cs
+++ src/RhythmUI/RhythmUI.SharedProject/Selection/SelectRoomInLinkNodeViewCustomization.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if !Revit2020 && Revit2021
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,3 +27,4 @@ namespace RhythmUI.Selection
         }
     }
 }
+#endif
