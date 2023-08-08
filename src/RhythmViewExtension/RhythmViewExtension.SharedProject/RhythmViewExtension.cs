932fbd579c6cbfcf4e0972c8495cd484b8eac8fc
John <sixtysecondrevit@gmail.com>
2023-08-08 16:19:02 -0600

    View extension code.

---
+++ src/RhythmViewExtension/RhythmViewExtension.SharedProject/RhythmViewExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Dynamo.Controls;
+using Dynamo.Graph.Nodes;
+using Dynamo.Graph.Workspaces;
+using Dynamo.Models;
+using Dynamo.ViewModels;
+using Dynamo.Wpf.Extensions;
+
+namespace RhythmViewExtension
+{
+    public class RhythmViewExtension : IViewExtension
+    {
+        public string UniqueId => "5435824A-A3A1-4FC1-AF42-E5139041740F";//NOTE: If you are building your own view extension, you MUST change this.
+        public string Name => "Rhythm View Extension";//NOTE: If you are building your own view extension, you MUST change this.
+
+
+        private readonly string _executingLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+        public void Dispose()
+        {
+        }
+        public static DynamoView view;
+
+        public void Startup(ViewStartupParams p)
+        {
+
+        }
+
+        private ViewLoadedParams loaded = null;
+        public void Loaded(ViewLoadedParams p)
+        {
+            loaded = p;
+            view = p.DynamoWindow as DynamoView;
+
+            //load the pertinent Rhythm library if it isn't loaded yet.
+            //p.ViewStartupParams.LibraryLoader.LoadNodeLibrary();
+
+            //Subscribe to node placed events for renaming stuff
+            p.CurrentWorkspaceChanged += POnCurrentWorkspaceChanged;
+            p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModelOnNodeAdded;
+        }
+        private static IEnumerable<Type> GetTypesSafely(Assembly assembly)
+        {
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                return ex.Types.Where(x => x != null);
+            }
+        }
+
+        private void POnCurrentWorkspaceChanged(IWorkspaceModel obj)
+        {
+            obj.NodeAdded -= CurrentWorkspaceModelOnNodeAdded;
+            obj.NodeAdded += CurrentWorkspaceModelOnNodeAdded;
+        }
+
+        private void CurrentWorkspaceModelOnNodeAdded(NodeModel obj)
+        {
+            string creationName = obj.CreationName;
+
+            if (creationName.Contains("Rhythm") && !obj.Name.Contains("ʳʰʸᵗʰᵐ|"))
+            {
+                obj.Name = "ʳʰʸᵗʰᵐ|" + obj.Name;
+            }
+            if (creationName.Contains("CloseDocument") || creationName.Contains("UpgradeFamilies"))
+            {
+                dynView.HomeSpace.RunSettings.RunType = RunType.Manual;
+            }
+        }
+
+
+        public void Shutdown()
+        {
+        }
+
+
+        // ReSharper disable once InconsistentNaming
+        public static DynamoViewModel dynView => view.DataContext as DynamoViewModel;
+
+    }
+}
