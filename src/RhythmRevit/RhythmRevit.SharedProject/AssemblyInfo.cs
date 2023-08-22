b7060d2c4f822af11b0d7224ec7b1084ea4ac0be
john pierson <sixtysecondrevit@gmail.com>
2023-08-22 09:24:23 -0600

    Rhythm version fixing (#57)
    * Revit 2024 project setup
    
    * Roll out 2020-2023 for version specific builds
    
    * View extension code.
    
    * Extension progress.
    
    * Extension appears to be loaded the package on the fly (even for Dynamo player)
    
    * Extension progress for now.
    
    * Hmm, writing DLLs on the fly is not working so well.
    
    * Currently getting loaded on the fly with a view extension and extension.
    
    * Getting close.
    
    * View extension testing.
    
    * Fix message
    
    * Compile
    
    * Cleanup usings, add warning suppression for #65
    
    * Restructure project codebase
    
    * Add folders to solution

---
+++ src/RhythmRevit/RhythmRevit.SharedProject/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3960ff23-2b1c-48ae-b770-d9203640fe07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2023.9.1")]
+[assembly: AssemblyFileVersion("2023.9.1")]
