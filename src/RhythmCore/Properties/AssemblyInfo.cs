2c13ff83e5149a1f6e08829e47589eed6bec2ad0
John <sixtysecondrevit@gmail.com>
2023-11-21 15:51:14 -0700

    Core Builds

--- src/RhythmCore/Properties/AssemblyInfo.cs
+++ src/RhythmCore/Properties/AssemblyInfo.cs
@@ -2,10 +2,60 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("63d58255-d8ab-42fe-bd8e-5ebe6c64c558")]
+[assembly: Guid("f6a30ee1-c5bc-44ab-a311-7c23cac88028")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2023.12.1")]
+[assembly: AssemblyFileVersion("2023.12.1")]
+
+#if R20
+[assembly: AssemblyTitle("Rhythm for Dynamo| Core 2020")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Core 2020")]
+#endif
+
+#if R21
+[assembly: AssemblyTitle("Rhythm for Dynamo| Core 2021")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Core 2021")]
+#endif
+
+#if R22
+[assembly: AssemblyTitle("Rhythm for Dynamo| Core 2022")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Core 2022")]
+#endif
+
+#if R23
+[assembly: AssemblyTitle("Rhythm for Dynamo| Core 2023")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Core 2023")]
+#endif
+
+#if R24
+[assembly: AssemblyTitle("Rhythm for Dynamo| Core 2024")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Core 2024")]
+#endif
+
+#if R25
+[assembly: AssemblyTitle("Rhythm for Dynamo| Core 2025")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Core 2025")]
+#endif
