ab31a6c5f83352e209664d7dfa245282da0367b0
John <sixtysecondrevit@gmail.com>
2023-11-21 16:05:28 -0700

    View Extension and Extension migration.

--- src/RhythmExtension/Properties/AssemblyInfo.cs
+++ src/RhythmExtension/Properties/AssemblyInfo.cs
@@ -2,6 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -9,3 +19,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("21e81214-dece-4c4a-8914-64e01f386613")]
+
+[assembly: AssemblyVersion("2023.12.1")]
+[assembly: AssemblyFileVersion("2023.12.1")]
--- src/RhythmViewExtension/Properties/AssemblyInfo.cs
+++ src/RhythmViewExtension/Properties/AssemblyInfo.cs
@@ -2,6 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -9,3 +19,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0b4b05e9-c1a8-4767-8307-98914bcc6842")]
+
+
+[assembly: AssemblyVersion("2023.12.1")]
+[assembly: AssemblyFileVersion("2023.12.1")]
