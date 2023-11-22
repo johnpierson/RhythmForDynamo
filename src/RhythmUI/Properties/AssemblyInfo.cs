571eb73aa03992774c828ac4c1906bdcaa5d94ea
John <sixtysecondrevit@gmail.com>
2023-11-22 13:36:54 -0700

    Modernize and move RhythmUI to .net7

--- src/RhythmUI/Properties/AssemblyInfo.cs
+++ src/RhythmUI/Properties/AssemblyInfo.cs
@@ -2,20 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RhythmUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RhythmUI")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -25,12 +23,39 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2023.8.2")]
-[assembly: AssemblyFileVersion("2023.8.2")]
+[assembly: AssemblyVersion("2023.12.1")]
+[assembly: AssemblyFileVersion("2023.12.1")]
+
+#if R20
+[assembly: AssemblyTitle("Rhythm for Dynamo| Revit 2020")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Revit 2020")]
+#endif
+
+#if R21
+[assembly: AssemblyTitle("Rhythm for Dynamo| Revit 2021")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Revit 2021")]
+#endif
+
+#if R22
+[assembly: AssemblyTitle("Rhythm for Dynamo| Revit 2022")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Revit 2022")]
+#endif
+
+#if R23
+[assembly: AssemblyTitle("Rhythm for Dynamo| Revit 2023")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Revit 2023")]
+#endif
+
+#if R24
+[assembly: AssemblyTitle("Rhythm for Dynamo| Revit 2024")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Revit 2024")]
+#endif
+
+#if R25
+[assembly: AssemblyTitle("Rhythm for Dynamo| Revit 2025")]
+[assembly: AssemblyProduct("Rhythm for Dynamo| Revit 2025")]
+#endif
