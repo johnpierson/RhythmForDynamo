0537a211c873a0770f4fa758325b9cefef5e278e
john pierson <john.pierson@autodesk.com>
2025-06-27 09:41:04 -0600

    Update viewport pack

--- src/RhythmCore/GenerativeDesign/GenerativeDesign.cs
+++ src/RhythmCore/GenerativeDesign/GenerativeDesign.cs
@@ -16,16 +16,17 @@ namespace Rhythm.GenerativeDesign
         }
 
         /// <summary>
-        /// 
+        /// Packs viewport rectangles within a container rectangle, ensuring no overlap with container edges
         /// </summary>
-        /// <param name="container"></param>
-        /// <param name="viewportRectangles"></param>
-        /// <param name="viewportIds"></param>
+        /// <param name="container">The container rectangle to pack viewports within</param>
+        /// <param name="viewportRectangles">List of viewport rectangles to pack</param>
+        /// <param name="viewportIds">List of viewport IDs corresponding to the rectangles</param>
+        /// <param name="marginFromEdge">Minimum margin distance from container edges</param>
         /// <returns name="viewportsThatFit">The viewports that fit in the titleblock container</returns>
         /// <returns name="proposedLocations">The proposed locations</returns>
         /// <returns name="viewportRectangles">The rectangles</returns>
         [MultiReturn(new[] { "viewportsThatFit", "proposedLocations", "viewportRectangles" })]
-        public static Dictionary<string, object> PackViewports(Rectangle container, List<Rectangle> viewportRectangles, List<int> viewportIds)
+        public static Dictionary<string, object> PackViewports(Rectangle container, List<Rectangle> viewportRectangles, List<int> viewportIds, double marginFromEdge)
         {
             List<int> viewportsThatFit = new List<int>();
             List<Point> points = new List<Point>();
@@ -33,9 +34,26 @@ namespace Rhythm.GenerativeDesign
 
             var containerBbox = container.BoundingBox;
             var containerWidth = containerBbox.MaxPoint.X - containerBbox.MinPoint.X;
-            var containerHeight = containerBbox.MaxPoint.X - containerBbox.MinPoint.X;
+            var containerHeight = containerBbox.MaxPoint.Y - containerBbox.MinPoint.Y; // Fixed: was using X instead of Y
+            
+            // Apply margin to reduce effective packing area
+            var effectiveWidth = containerWidth - (2 * marginFromEdge);
+            var effectiveHeight = containerHeight - (2 * marginFromEdge);
 
-            CygonRectanglePacker packer = new CygonRectanglePacker(containerWidth, containerHeight);
+            // Ensure effective dimensions are positive
+            if (effectiveWidth <= 0 || effectiveHeight <= 0)
+            {
+                // Return empty results if margin is too large for container
+                var emptyInfo = new Dictionary<string, object>
+                {
+                    {"viewportsThatFit", viewportsThatFit},
+                    { "proposedLocations", points},
+                    { "viewportRectangles", rects}
+                };
+                return emptyInfo;
+            }
+
+            CygonRectanglePacker packer = new CygonRectanglePacker(effectiveWidth, effectiveHeight);
 
             UV placement = null;
             for (int i = 0; i < viewportRectangles.Count; i++)
@@ -66,10 +84,15 @@ namespace Rhythm.GenerativeDesign
                         bottomLeftX = 0;
                         bottomLeftY = 0;
                     }
+                    
+                    // Account for margin offset in the placement calculation
+                    var adjustedX = placement.U + bottomLeftX + marginFromEdge + width / 2;
+                    var adjustedY = placement.V + bottomLeftY + marginFromEdge + height / 2;
+                    
                     viewportsThatFit.Add(viewportIds[i]);
                     rects.Add(current);
 
-                    points.Add(Point.ByCoordinates(placement.U + bottomLeftX + width / 2, placement.V + bottomLeftY + height / 2, 0));
+                    points.Add(Point.ByCoordinates(adjustedX, adjustedY, 0));
                 }
             }
             //returns the outputs
